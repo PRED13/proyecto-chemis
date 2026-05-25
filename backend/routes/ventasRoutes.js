b6ee7dc7851b09a8019bae3a0270c27118ca67f8
PRED13 <155868501+PRED13@users.noreply.github.com>
2026-05-25 09:18:02 -0600

    Delete backend directory

--- backend/routes/ventasRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { completarVenta, obtenerHistorial } = require('../controllers/ventasController');
-const { verificarToken } = require('../controllers/authController');
-
-router.post('/completar', verificarToken, completarVenta);
-router.get('/historial', verificarToken, obtenerHistorial); // Nueva ruta
-
-module.exports = router;
